33828796d18a75277f34a3e95c082051f54e6901
Socks <patricksutcliffe123@gmail.com>
2021-10-28 10:02:17 -0400

    got the api to return the correct data based on the models

--- seeds/eventUser-seeds.js
+++ seeds/eventUser-seeds.js
@@ -1,38 +1,21 @@
-const { eventUser } = require('../models');
+const { EventUser } = require('../models');
 
 const eventUserData = [
     {
-        users_id: 1,
-        events_id: 1
+        usersId: 1,
+        eventsId: 1
     },
     {
-        users_id: 2,
-        events_id: 1
+        usersId: 2,
+        eventsId: 1
     },
     {
-        users_id: 3,
-        events_id: 1
+        usersId: 3,
+        eventsId: 1
     }
 ]
 
-const seedEventUser = () => eventUser.bulkCreate(eventUserData)
-
-router.delete('/:id', (req, res) => {
-    eventUser.destroy({
-      where: {
-        id: req.params.id,
-      },
-    })
-      .then((products) => {
-        console.log(products);
-        res.json(products);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  });
-  
+const seedEventUser = () => EventUser.bulkCreate(eventUserData)
 
 module.exports = seedEventUser
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-
+const helpers = require('./utils/auth')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
