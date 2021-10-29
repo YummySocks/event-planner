b0aae21542b16cd326e65e227d98d8720ec1bbca
Socks <patricksutcliffe123@gmail.com>
2021-10-29 10:42:31 -0400

    pulling main

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,43 +5,6 @@ const userRoute = require('./user-routes')
 router.use('/event', eventRoute)
 router.use('/users', userRoute)
 
-const deleteEvent = (id) =>
-  fetch(`/api/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  const saveEvent = (event) =>
-  fetch(`/api/${event}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(event),
-  });
-
-  const getEvent = (id) =>
-  fetch(`/api/${id}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  const handleEventSave = () => {
-    const newEvent = {
-      title: title.value,
-      description: description.value,
-      dates: dates.value,
-      capacity: capacity.value
-    };
-    saveEvent(newEvent).then(() => {
-      //getAndRenderEvent();
-      //renderActiveEvent();
-    });
-  };
-
+router.put()
 
   module.exports = router;
